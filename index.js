b854236641fb4ba0425125b45f7368410272973f
Moon-sang ho <skym887@gmail.com>
2021-08-30 10:56:11 +0900

    fix: Downgrade react-native version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { name as appName } from './app.json';
 import App from './src/App';
 import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
