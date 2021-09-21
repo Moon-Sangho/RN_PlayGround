8747655868a5b4ca59bb75d5b3faf75bd8b604c0
Moon-sang ho <skym887@gmail.com>
2021-09-22 00:33:59 +0900

    feat: add roulette  style & function

--- src/screens/RouletteScreen.tsx
+++ src/screens/RouletteScreen.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styled from '@emotion/native';
 import roulettePin from 'assets/images/roulette-pin.svg';
-import Animated from 'react-native-reanimated';
+import { Animated, Easing } from 'react-native';
 import Svg, { Circle, SvgXml } from 'react-native-svg';
 import Btn from 'src/components/atoms/Btn';
 
 const RouletteScreen = () => {
+  const roulette = useRef(new Animated.Value(0)).current;
+
+  const createRandomDegree = () => {
+    return Math.floor(Math.random() * 360) + 1080; // 3 바퀴 이상 돌리기 위해 1080도 추가
+  };
+
+  const [degree, setDegree] = useState(0);
+  const [nextDegree, setNextDegree] = useState(degree + createRandomDegree());
+
+  const interpolateRotation = roulette.interpolate({
+    inputRange: [0, 1],
+    outputRange: [`${degree}deg`, `${nextDegree}deg`],
+  });
+
+  const rouletteStyle = {
+    transform: [
+      {
+        rotate: interpolateRotation,
+      },
+    ],
+  };
+
+  const rotateRoulette = () => {
+    roulette.setValue(0);
+    setDegree(nextDegree);
+    setNextDegree(nextDegree + createRandomDegree());
+
+    Animated.timing(roulette, {
+      toValue: 1,
+      duration: 1500,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <Container>
       <Roulette>
         <RoulettePin>
           <SvgXml xml={roulettePin} />
         </RoulettePin>
-        <Animated.View>
+        <Animated.View style={rouletteStyle}>
           <SvgWrapper>
             <Svg height="100" width="100">
               <Circle cx="100" cy="100" r="100" fill="pink" />
@@ -31,7 +66,7 @@ const RouletteScreen = () => {
           </SvgWrapper>
         </Animated.View>
       </Roulette>
-      <Btn width={100} backgroundColor="gray">
+      <Btn width={100} backgroundColor="gray" onPress={rotateRoulette}>
         룰렛 돌리기
       </Btn>
     </Container>
