42f608995dc2427651738afa50c9ad0414db7a57
Moon-sang ho <skym887@gmail.com>
2021-09-28 21:00:13 +0900

    feat: add scroll animation template

---
+++ src/components/templates/ScrollAnimationTemplate.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { LayoutRectangle, View } from 'react-native';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useDerivedValue,
+} from 'react-native-reanimated';
+
+export type ScrollAnimationViewProps = {
+  scroll: Animated.SharedValue<number>;
+  windowHeight: number;
+  children: (
+    progress: Readonly<Animated.SharedValue<number>>,
+    scroll: Animated.SharedValue<number>,
+    layout: LayoutRectangle | undefined,
+  ) => JSX.Element;
+  offset?: number;
+};
+
+const ScrollAnimationTemplate = ({
+  scroll,
+  windowHeight,
+  children,
+  offset = 0,
+}: ScrollAnimationViewProps) => {
+  const [layout, setLayout] = useState<LayoutRectangle>();
+
+  const progress = useDerivedValue(() => {
+    return layout
+      ? interpolate(
+          scroll.value,
+          [layout.y - windowHeight, layout.y - windowHeight + offset],
+          [0, 1],
+          Extrapolate.CLAMP,
+        )
+      : 0;
+  });
+
+  return (
+    <View onLayout={e => setLayout(e.nativeEvent.layout)}>
+      {children(progress, scroll, layout)}
+    </View>
+  );
+};
+
+export default ScrollAnimationTemplate;
